289eafa1f4563d49e7d9c902eadd50a39c156d1d
Elaiza Endrinal <endrinalkyla8@gmail.com>
2024-01-06 23:14:04 +0800

    Done styling the Tab 1 (Input)

--- Biggest_Number.py
+++ Biggest_Number.py
@@ -17,13 +17,23 @@ notebook = ttk.Notebook(window, style='MyStyle.TNotebook')
 tab_input = ttk.Frame(notebook)
 notebook.add(tab_input, text='Input')
 
+# Create a notebook (tabs) style
+style = ttk.Style()
+style.theme_create("MyStyle", parent="alt", settings={
+    "TNotebook": {"configure": {"tabmargins": [0, 5, 0, 0], "background": "#FFC0CB"}},
+    "TNotebook.Tab": {
+        "configure": {"padding": [10, 5], "background": "#FF69B4", "font": ('Helvetica', 10, 'bold')},
+        "map": {"background": [("selected", "#FFD700")]}}})
+style.theme_use("MyStyle")
+
 # Pack the notebook to make it visible
 notebook.pack(expand=1, fill='both')
 
 # Run the main loop
 window.mainloop()
 
-# Create a notebook (tabs) style
+
+
 # Tab to enter first, second, and third number
 # Define the function to find the biggest number
 # Create the Tab 2 - Result
